2ef45304ac49d1b88f0de587c5c24ad41e0cf35a
Francois Blackburn <fblackburn@wazo.community>
2025-09-17 07:11:26 -0400

    run pyupgrade

--- wazo_amid_client/types.py
+++ wazo_amid_client/types.py
@@ -1,10 +1,8 @@
-# Copyright 2024 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2024-2025 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 from __future__ import annotations
 
-from typing import Union
-
-from typing_extensions import TypeAlias
+from typing import TypeAlias, Union
 
 JSON: TypeAlias = Union[str, int, float, bool, None, list['JSON'], dict[str, 'JSON']]
